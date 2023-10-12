07cbbd433ff5bfb0b6c0b26617a9e436f9c58bfd
Ramil <Warkinstar@mail.ru>
2023-10-12 22:23:55 +0600

    User Profile - UserUpdateView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from .views import UserUpdateView
 
 
 urlpatterns = [
     # all path of django-allauth
     path("", include("allauth.urls")),
+    path("profile/<pk>/", UserUpdateView.as_view(), name="user_update"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import UpdateView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-# Create your views here.
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = get_user_model()
+    fields = ["username", "email", "tg_user_id"]
+    template_name = "account/user_update.html"
+    success_url = "/"
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj
